46e2619fa8946ab562dfd40672da6b438c44a433
rythorpe <ryan_thorpe@brown.edu>
2022-06-21 19:44:20 -0400

    fix job name

--- sensitivity_mn_sbatch.sh
+++ sensitivity_mn_sbatch.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Request an hour of runtime:
-
+#SBATCH -J sensitivity_job
 #SBATCH -n 24
 ##SBATCH --mem=40g --ntasks-per-node=16 -N 25
 #SBATCH --mem=40G
 #SBATCH -t 24:00:00
 #SBATCH -A carney-sjones-condo
-
-# Specify a job name:
-SBATCH -J sensitivity_batch
 
 
 # module load python/3.7.4 mpi/openmpi_4.0.5_gcc_10.2_slurm20 gcc/10.2
